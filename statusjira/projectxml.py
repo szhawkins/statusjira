f874832bdd5d87ece43db25bd5523044cce257ee
Steve Hawkins <shawkins@legacy-sw.com>
2016-05-13 12:52:27 -0700

    Begin file parsing logic

--- statusjira/projectxml.py
+++ statusjira/projectxml.py
@@ -16,13 +16,20 @@ class file (object):
         self.__fileName = "" #Filename property remains empty until file is successfully loaded
 
         try:
-            self.xmlTree = ET.parse(fileName)
+            self.__xmlTree = ET.parse(fileName)
             self.__fileName = fileName
         except IOError:
             self.__fileName = "" #If file is not loaded, filename property remains empty
 
     def fileName(self):
         return self.__fileName
+
+    def tmp (self):
+        root = self.__xmlTree.getroot() #Returns the root element
+        print "Root tag: ", root.tag
+
+        for ff in root.findall("./channel/item"):
+            print "ff tag: ", ff.tag
 
 
 
--- test/test_projectxml.py
+++ test/test_projectxml.py
@@ -1,10 +1,11 @@
 import unittest
+import os
 from statusjira import projectxml as prj
 
 class TestProjectXML(unittest.TestCase):
 
     def setUp(self):
-        self.__testFile1 = "JiraProjectTestData.xml"
+        self.__testFile1 = "test/JiraProjectTestData.xml"
 
         self.projectFile = prj.file()
 
@@ -12,6 +13,17 @@ class TestProjectXML(unittest.TestCase):
         self.projectFile.load (self.__testFile1)
 
         self.assertEquals (self.__testFile1, self.projectFile.fileName())
+
+    def test_isLoaded(self):
+        self.assertFalse(self.projectFile.fileName())  #Empty string resolves to false
+        self.projectFile.load (self.__testFile1)       #Load a project file
+        self.assertTrue(self.projectFile.fileName())   #Non-Empty string resolves to true
+
+    def test_tmp(self):        
+        self.projectFile.load (self.__testFile1)       #Load a project file
+
+        self.projectFile.tmp();
+        
 
 if __name__ == '__main__':
     unittest.main()
